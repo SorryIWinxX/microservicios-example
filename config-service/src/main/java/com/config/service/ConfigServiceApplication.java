9ed8a0b53ea7ef2ae0bd3335cbb9059172ae484a
Daniel La Rotta <daniel2191937@correo.uis.edu.co>
2023-01-09 10:50:58 -0500

    config server

--- config-service/src/main/java/com/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/config/service/ConfigServiceApplication.java
@@ -3,7 +3,7 @@ package com.config.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {
 
